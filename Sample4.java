1f88a9dacf1fe1203207ac1a2d88c108f7c98252
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:36:49 +0530

    Add files via upload

---
+++ Sample4.java
@@ -0,0 +1,10 @@
+ class Sample4{
+  public static void main(String args[]) {
+    int a=4,b=3;
+
+int value=(int)Math.pow(a,2)+(int)Math.pow(b,2);
+
+System.out.println("Sum of squares of two numbers="+value);
+  }
+}
+  
