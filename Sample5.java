4d98edb4c551a3d796e9c219856164f7c224dc86
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:36:25 +0530

    Add files via upload

---
+++ Sample5.java
@@ -0,0 +1,9 @@
+// area of tringle
+class Sample5{
+   public static void main(String args[]) {
+     int b=8,h =13 ;
+     int area_tringle=( b*h) / 2;
+System.out.println("Area of the tringle="+area_tringle);
+ 
+   }
+}
