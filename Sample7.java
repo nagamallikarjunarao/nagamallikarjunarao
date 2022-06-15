57cf0cfcdfd169050d89ed6fb17bdabdd26fbbe2
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:35:26 +0530

    Add files via upload

---
+++ Sample7.java
@@ -0,0 +1,10 @@
+//Area of the rectangle
+class Sample7{
+  public static void main(String args[]) {
+   
+int l=13,w=10;
+int area_rectangle=l*w;
+System.out.println("Area of the rectangle="+area_rectangle); 
+  }
+  
+}
