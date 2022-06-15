6ce0ea9ce44ca4dd19d3bbd2c6029460b9da483c
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:35:07 +0530

    Add files via upload

---
+++ Sample8.java
@@ -0,0 +1,10 @@
+//perimeter of the rectangle
+class Sample8{
+  public static void main(String args[]) {
+   
+int l=13,w=10;
+int perimeter_rectangle=(2*(l+w));
+System.out.println("perimeter of the rectangle="+perimeter_rectangle); 
+  }
+  
+}
