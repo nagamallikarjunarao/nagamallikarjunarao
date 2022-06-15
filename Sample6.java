89bc310b119860b4a17db60ee90c7c8eda4482b2
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:36:03 +0530

    Add files via upload

---
+++ Sample6.java
@@ -0,0 +1,15 @@
+//area of sqaure
+
+class Sample6{
+  public static void main(String args[]) {
+   
+int a=13;
+int area_square=a*a;
+System.out.println("Area of the square="+area_square); 
+  }
+  
+}
+
+
+
+
