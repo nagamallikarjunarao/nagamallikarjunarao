776f935c68c13af3da0d85d2f6fef1c891d01ce0
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-15 12:34:27 +0530

    Add files via upload

---
+++ Sample9.java
@@ -0,0 +1,10 @@
+//erimeter of the square
+class Sample9{
+  public static void main(String args[]) {
+   
+int a=12;
+int perimeter_square=(4*a);
+System.out.println("perimeter of the square="+perimeter_square); 
+  }
+  
+}
